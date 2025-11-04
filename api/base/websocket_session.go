e4b935ada03cbce0b5ea637e7460b96542670759
qiuyonggang <qiuyonggang@chuanyuapp.com>
2025-11-04 19:05:46 +0800

    add log

--- api/base/websocket_session.go
+++ api/base/websocket_session.go
@@ -166,7 +166,7 @@ func (ws *WebSocketSession) SaveSessionId(sessionId string) error {
 	}
 	err = os.WriteFile(ws.SessionFile, data, 0644)
 	if err != nil {
-		log.WithError(err).WithField("data", data).Error("SaveSessionId")
+		log.WithError(err).WithField("sessionId", sessionId).Error("SaveSessionId")
 		return err
 	}
 	return nil
